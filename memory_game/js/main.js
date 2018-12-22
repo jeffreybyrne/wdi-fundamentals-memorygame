69864d1d9e8a6a28e3cb474013e81e154cce898c
Jeff Byrne <jeffrey.byrne@gmail.com>
2018-12-22 16:02:38 -0500

    added the rest of the functionality for the game, a score tracker, and a reset button

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,21 +1,22 @@
 //Array of cards
-var cards = [{rank: "queen", suit: "hearts", cardImage: "images/queen-of-hearts.png"},
-{rank: "queen", suit: "diamonds", cardImage: "images/queen-of-diamonds.png"},
-{rank: "king", suit: "hearts", cardImage: "images/king-of-hearts.png"},
-{rank: "king", suit: "diamonds", cardImage: "images/king-of-diamonds.png"}];
+var cards = [
+	{rank: "queen", suit: "hearts", cardImage: "images/queen-of-hearts.png"},
+	{rank: "queen", suit: "diamonds", cardImage: "images/queen-of-diamonds.png"},
+	{rank: "king", suit: "hearts", cardImage: "images/king-of-hearts.png"},
+	{rank: "king", suit: "diamonds", cardImage: "images/king-of-diamonds.png"}];
 
 //Array of the cards currently in play
 var cardsInPlay = [];
 
 //Function to determine what the user selected
-var flipCard = function(cardId) {
-	//Log which card the user selected
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
-	//Add the selected card to the list of cards currently in play
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
-	//If there are now two cards in play, check for a match
+	this.setAttribute('src',cards[cardId].cardImage);
+
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}		
@@ -25,10 +26,54 @@ var flipCard = function(cardId) {
 var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert('You found a match!');
+		wins++;
+		addScore('win');
 	} else {
 		alert('Sorry, try again.');
+		losses++;
+		addScore('loss')
+	}
+	
+}
+
+//Function to create a board
+var createBoard = function() {
+	for (i=0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
 	}
 }
 
-flipCard(0);
-flipCard(2);
+//Function to reset the board, it clears the HTML of the current board, resets the cards in play, and creates a new board
+var clearBoard = function() {
+	var board = document.getElementById('game-board');
+	board.innerHTML = '';
+	cardsInPlay = [];
+	createBoard();
+}
+
+//Adds a listener to the reset button to call the right function
+var resetButton = document.getElementsByTagName('button');
+resetButton[0].addEventListener('click', clearBoard);
+
+//Initialize the board and start tracking game stats
+createBoard();
+var gameNumber = 1;
+var wins = 0;
+var losses = 0;
+var currentScore = '0-0';
+var scoreLocation = document.getElementById('score-location');
+var score = document.getElementById('score');
+
+//Function to determine the score after the end of a game
+var addScore = function(outcome) {
+	var scoreElement = document.createElement('li');
+	scoreElement.textContent = 'Game ' + gameNumber + ' was a ' + outcome + '.';
+	scoreElement.className = 'scoreList';
+	scoreLocation.appendChild(scoreElement);
+	score.textContent = 'Score: ' + wins + '-' + losses;
+	gameNumber++;
+}
